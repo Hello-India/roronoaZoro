9e6bed1aaf19af2e23c0d671f1899e90b7f56841
SAHILXOFFICIAL <134754968+Anything-bro@users.noreply.github.com>
2024-02-09 17:51:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/Anything-bro/RORONOA.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
 fi
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting RORONOA...."
 python3 bot.py
